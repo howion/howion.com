f5932c947c9e29c79152418f30a98d7f860ef771
howion <me@howion.com>
2023-03-26 19:15:14 +0300

    fix build error

--- pages/forks/timeis.tsx
+++ pages/forks/timeis.tsx
@@ -1,139 +1,140 @@
-import React, { useRef, useState } from 'react'
-import { Meta } from '/components/meta'
+export {}
+// import React, { useRef, useState } from 'react'
+// import { Meta } from '/components/meta'
 
-// IMAGES
-// import adimOdtuLogo from '/public/img/sponsor/adimodtu.png'
-import { useDidMount } from 'rooks'
-import { TransitorService } from '/services/transitor.service'
+// // IMAGES
+// // import adimOdtuLogo from '/public/img/sponsor/adimodtu.png'
+// import { useDidMount } from 'rooks'
+// import { TransitorService } from '/services/transitor.service'
 
-function formatPrefixZero(n: number): text {
-    return n < 10 ? `0${n}` : `${n}`
-}
+// function formatPrefixZero(n: number): text {
+//     return n < 10 ? `0${n}` : `${n}`
+// }
 
-function formatTime(date: Date) {
-    const h = date.getHours()
-    const m = date.getMinutes()
-    const s = date.getSeconds()
+// function formatTime(date: Date) {
+//     const h = date.getHours()
+//     const m = date.getMinutes()
+//     const s = date.getSeconds()
 
-    return `${formatPrefixZero(h)}:${formatPrefixZero(m)}:${formatPrefixZero(s)}`
-}
+//     return `${formatPrefixZero(h)}:${formatPrefixZero(m)}:${formatPrefixZero(s)}`
+// }
 
-interface TimerCharProps {
-    char: text
-}
+// interface TimerCharProps {
+//     char: text
+// }
 
-function TimerChar(props: TimerCharProps): FCReturn {
-    const ref = useRef<HTMLDivElement>(null)
+// function TimerChar(props: TimerCharProps): FCReturn {
+//     const ref = useRef<HTMLDivElement>(null)
 
-    if (props.char === ':') {
-        return <h1 className="forks-timeis-timer-char forks-timeis-timer-char-colon"></h1>
-    }
+//     if (props.char === ':') {
+//         return <h1 className="forks-timeis-timer-char forks-timeis-timer-char-colon"></h1>
+//     }
 
-    const number = Number.parseInt(props.char)
-}
+//     const number = Number.parseInt(props.char)
+// }
 
-export default function ForksTimeIs(): FCReturn {
-    const [date, setDate] = useState(new Date())
+// export default function ForksTimeIs(): FCReturn {
+//     const [date, setDate] = useState(new Date())
 
-    useDidMount(() => {
-        TransitorService.hideTransitor()
-    })
+//     useDidMount(() => {
+//         TransitorService.hideTransitor()
+//     })
 
-    return (
-        <>
-            <Meta />
-            <div className="forks-timeis-timer">
-                <div className="forks-timeis-timer-next"></div>
-            </div>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <Meta />
+//             <div className="forks-timeis-timer">
+//                 <div className="forks-timeis-timer-next"></div>
+//             </div>
+//         </>
+//     )
+// }
 
-interface TimerCharProps {
-    char: string
-}
+// interface TimerCharProps {
+//     char: string
+// }
 
-const TimerChar: React.FC<TimerCharProps> = (props: TimerCharProps) => {
-    const ref: React.MutableRefObject<HTMLDivElement> = React.useRef<HTMLDivElement>(null)
+// const TimerChar: React.FC<TimerCharProps> = (props: TimerCharProps) => {
+//     const ref: React.MutableRefObject<HTMLDivElement> = React.useRef<HTMLDivElement>(null)
 
-    const colon: boolean = props.char === ':'
+//     const colon: boolean = props.char === ':'
 
-    if (colon) {
-        return <h1 className="timer-char colon">:</h1>
-    }
+//     if (colon) {
+//         return <h1 className="timer-char colon">:</h1>
+//     }
 
-    const number: number = parseInt(props.char)
+//     const number: number = parseInt(props.char)
 
-    const getCharSlider = (): JSX.Element[] => {
-        let options: JSX.Element[] = []
+//     const getCharSlider = (): JSX.Element[] => {
+//         let options: JSX.Element[] = []
 
-        for (let i: number = 0; i <= 9; i++) {
-            const classes: string = classNames('timer-char-slider-option', {
-                active: number === i
-            })
+//         for (let i: number = 0; i <= 9; i++) {
+//             const classes: string = classNames('timer-char-slider-option', {
+//                 active: number === i
+//             })
 
-            options.push(
-                <span key={i} className={classes}>
-                    {i}
-                </span>
-            )
-        }
+//             options.push(
+//                 <span key={i} className={classes}>
+//                     {i}
+//                 </span>
+//             )
+//         }
 
-        const height: number = ref.current ? ref.current.offsetHeight : 0,
-            top: string = `${number * height * -1}px`
+//         const height: number = ref.current ? ref.current.offsetHeight : 0,
+//             top: string = `${number * height * -1}px`
 
-        return (
-            <div className="timer-char-slider" style={{ top }}>
-                {options}
-            </div>
-        )
-    }
+//         return (
+//             <div className="timer-char-slider" style={{ top }}>
+//                 {options}
+//             </div>
+//         )
+//     }
 
-    return (
-        <div ref={ref} className="timer-char number">
-            {getCharSlider()}
-        </div>
-    )
-}
+//     return (
+//         <div ref={ref} className="timer-char number">
+//             {getCharSlider()}
+//         </div>
+//     )
+// }
 
-const Timer: React.FC = () => {
-    const [date, setDateTo] = React.useState<Date>(new Date())
+export default function Timer(): FCReturn {
+    // const [date, setDateTo] = React.useState<Date>(new Date())
 
-    React.useEffect(() => {
-        const interval: NodeJS.Timeout = setInterval(() => {
-            const update: Date = new Date()
+    // React.useEffect(() => {
+    //     const interval: NodeJS.Timeout = setInterval(() => {
+    //         const update: Date = new Date()
 
-            if (update.getSeconds() !== date.getSeconds()) {
-                setDateTo(update)
-            }
-        }, 100)
+    //         if (update.getSeconds() !== date.getSeconds()) {
+    //             setDateTo(update)
+    //         }
+    //     }, 100)
 
-        return () => {
-            clearInterval(interval)
-        }
-    }, [date])
+    //     return () => {
+    //         clearInterval(interval)
+    //     }
+    // }, [date])
 
-    const formatSegment = (segment: number): string => {
-        return segment < 10 ? `0${segment}` : segment
-    }
+    // const formatSegment = (segment: number): string => {
+    //     return segment < 10 ? `0${segment}` : segment
+    // }
 
-    const getTime = (): string => {
-        const hours: string = getHours(date.getHours()),
-            minutes: string = date.getMinutes(),
-            seconds: string = date.getSeconds()
+    // const getTime = (): string => {
+    //     const hours: string = getHours(date.getHours()),
+    //         minutes: string = date.getMinutes(),
+    //         seconds: string = date.getSeconds()
 
-        return `${formatSegment(hours)}:${formatSegment(minutes)}:${formatSegment(seconds)}`
-    }
+    //     return `${formatSegment(hours)}:${formatSegment(minutes)}:${formatSegment(seconds)}`
+    // }
 
-    const getChars = (): JSX.Element[] => {
-        return getTime()
-            .split('')
-            .map((char: string, index: number) => <TimerChar key={index} char={char} />)
-    }
+    // const getChars = (): JSX.Element[] => {
+    //     return getTime()
+    //         .split('')
+    //         .map((char: string, index: number) => <TimerChar key={index} char={char} />)
+    // }
 
     return (
         <div id="timer">
-            <div id="timer-text">{getChars()}</div>
+            <div id="timer-text">{/*getChars()*/}</div>
         </div>
     )
 }
